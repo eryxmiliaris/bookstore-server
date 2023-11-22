4e3a121f850083bff0d0b9a65c3f26210663b4e9
eryxmiliaris <eryxmiliaris232@protonmail.com>
2023-11-22 17:13:02 +0100

    Update book controller, add book image, popular books, categories and recommendations GET endpoints

--- src/main/java/com/vb/bookstore/controllers/BookController.java
+++ src/main/java/com/vb/bookstore/controllers/BookController.java
@@ -1,12 +1,14 @@
 package com.vb.bookstore.controllers;
 
+import com.vb.bookstore.payloads.PageableResponse;
 import com.vb.bookstore.payloads.books.BookDTO;
 import com.vb.bookstore.payloads.books.BookMainInfoDTO;
-import com.vb.bookstore.payloads.books.BookResponse;
+import com.vb.bookstore.payloads.books.CategoryDTO;
 import com.vb.bookstore.services.BookService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,7 +20,7 @@ public class BookController {
     private final BookService bookService;
 
     @GetMapping("/books")
-    public ResponseEntity<BookResponse> getAllBooks(
+    public ResponseEntity<PageableResponse> getAllBooks(
             @RequestParam(name = "pageNumber", defaultValue = "0", required = false) Integer pageNumber,
             @RequestParam(name = "pageSize", defaultValue = "12", required = false) Integer pageSize,
             @RequestParam(name = "sortBy", defaultValue = "id", required = false) String sortBy,
@@ -29,7 +31,7 @@ public class BookController {
             @RequestParam(name = "bookType", required = false) String[] bookTypes,
             @RequestParam(name = "bookTitle", required = false) String bookTitle
     ) {
-        BookResponse bookResponse = bookService.getBooksWithFilters(pageNumber, pageSize, sortBy, sortOrder, priceStart, priceEnd, categories, bookTypes, bookTitle);
+        PageableResponse bookResponse = bookService.getBooksWithFilters(pageNumber, pageSize, sortBy, sortOrder, priceStart, priceEnd, categories, bookTypes, bookTitle);
 
         return ResponseEntity.ok(bookResponse);
     }
@@ -37,7 +39,37 @@ public class BookController {
     @GetMapping("/books/{id}")
     public ResponseEntity<BookDTO> getBookById(@PathVariable Long id) {
         BookDTO bookDTO = bookService.getBookById(id);
-
         return ResponseEntity.ok(bookDTO);
+    }
+
+    @GetMapping(
+            value = "/books/{id}/coverImage",
+            produces = MediaType.IMAGE_JPEG_VALUE
+    )
+    public @ResponseBody byte[] getBookCoverImage(
+            @PathVariable Long id,
+            @RequestParam String bookType,
+            @RequestParam(required = false) Long paperBookId
+    ) {
+        return bookService.getBookCoverImage(id, bookType, paperBookId);
+    }
+
+    @GetMapping("/books/popular")
+    public ResponseEntity<List<BookMainInfoDTO>> getPopularBooks() {
+        List<BookMainInfoDTO> popularBooks = bookService.getPopularBooks();
+        return ResponseEntity.ok(popularBooks);
+    }
+
+    @GetMapping("/books/recommended")
+    @PreAuthorize("hasRole('USER')")
+    public ResponseEntity<List<BookMainInfoDTO>> getRecommendedBooks() {
+        List<BookMainInfoDTO> recommendedBooks = bookService.getRecommendedBooks();
+        return ResponseEntity.ok(recommendedBooks);
+    }
+
+    @GetMapping("/books/categories")
+    public ResponseEntity<List<CategoryDTO>> getAllCategories() {
+        List<CategoryDTO> categoryDTOS = bookService.getAllCategories();
+        return ResponseEntity.ok(categoryDTOS);
     }
 }
--- src/main/java/com/vb/bookstore/services/BookService.java
+++ src/main/java/com/vb/bookstore/services/BookService.java
@@ -1,40 +1,75 @@
 package com.vb.bookstore.services;
 
-import com.vb.bookstore.entities.Book;
-import com.vb.bookstore.entities.Category;
-import com.vb.bookstore.entities.PaperBook;
+import com.vb.bookstore.entities.*;
+import com.vb.bookstore.exceptions.ApiRequestException;
 import com.vb.bookstore.exceptions.ResourceNotFoundException;
-import com.vb.bookstore.payloads.books.BookDTO;
-import com.vb.bookstore.payloads.books.BookMainInfoDTO;
-import com.vb.bookstore.payloads.books.BookResponse;
-import com.vb.bookstore.repositories.BookRepository;
+import com.vb.bookstore.payloads.PageableResponse;
+import com.vb.bookstore.payloads.books.*;
+import com.vb.bookstore.repositories.*;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
-import org.springframework.data.domain.*;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.util.*;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 @Service
 @RequiredArgsConstructor
 public class BookService {
-    private final BookRepository bookRepository;
     private final ModelMapper modelMapper;
+    private final UserService userService;
+    private final RecommendationService recommendationService;
+
+    private final BookRepository bookRepository;
+    private final CategoryRepository categoryRepository;
+    private final PaperBookRepository paperBookRepository;
+    private final OrderRepository orderRepository;
+    private final WishlistRepository wishlistRepository;
 
     public BookDTO getBookById(Long id) {
+        boolean isAdmin = userService.currentUserIsAdmin();
         Book book = bookRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("Book", "bookId", id));
 
         BookDTO bookDTO = modelMapper.map(book, BookDTO.class);
+        List<PaperBookDTO> paperBookDTOs = new ArrayList<>();
+        for (PaperBook paperBook : book.getPaperBooks()) {
+            if (!paperBook.getIsHidden() || isAdmin) {
+                paperBookDTOs.add(modelMapper.map(paperBook, PaperBookDTO.class));
+            }
+        }
+        bookDTO.setPaperBooks(paperBookDTOs);
+        if (!isAdmin && book.getEbook() != null && book.getEbook().getIsHidden()) {
+            bookDTO.setEBook(null);
+        }
+        if (!isAdmin && book.getAudioBook() != null && book.getAudioBook().getIsHidden()) {
+            bookDTO.setAudioBook(null);
+        }
+
+        List<ReviewDTO> reviewDTOS = new ArrayList<>();
+        for (Review review : book.getReviews()) {
+            ReviewDTO reviewDTO = modelMapper.map(review, ReviewDTO.class);
+            reviewDTO.setUserName(review.getUser().getUsername());
+            reviewDTOS.add(reviewDTO);
+        }
+        bookDTO.setReviews(reviewDTOS);
         bookDTO.setCategories(book.getCategories().stream().map(Category::getCategoryName).toList());
 
         return bookDTO;
     }
 
-    public BookResponse getBooksWithFilters(
+    public PageableResponse getBooksWithFilters(
             Integer pageNumber,
             Integer pageSize,
             String sortBy,
@@ -55,8 +90,8 @@ public class BookService {
             categoriesList = Arrays.asList(categories);
         }
 
-        Double priceStartDouble = priceStart == null ? 0.0 : Double.parseDouble(priceStart);
-        Double priceEndDouble = priceEnd == null ? 100000.0 : Double.parseDouble(priceEnd);
+        double priceStartDouble = priceStart == null ? 0.0 : Double.parseDouble(priceStart);
+        double priceEndDouble = priceEnd == null ? 100000.0 : Double.parseDouble(priceEnd);
         Double priceStartPB = priceStartDouble;
         Double priceEndPB = priceEndDouble;
         Double priceStartEB = priceStartDouble;
@@ -90,6 +125,8 @@ public class BookService {
             }
         }
 
+        boolean includeHidden = userService.currentUserIsAdmin();
+
         Page<Book> books = bookRepository.findByFilterParams(
                 bookTitle
                 , hasCategories
@@ -97,12 +134,13 @@ public class BookService {
                 , priceStartPB, priceEndPB
                 , priceStartEB, priceEndEB
                 , priceStartAB, priceEndAB
-                ,pageDetails
+                , includeHidden
+                , pageDetails
         );
 
         List<BookMainInfoDTO> pageContentDTO = bookStreamToBookDtoList(books.stream());
 
-        BookResponse bookResponse = new BookResponse();
+        PageableResponse bookResponse = new PageableResponse();
         bookResponse.setContent(pageContentDTO);
         bookResponse.setPageNumber(pageNumber);
         bookResponse.setPageSize(pageSize);
@@ -112,19 +150,24 @@ public class BookService {
         return bookResponse;
     }
 
-    private List<BookMainInfoDTO> bookStreamToBookDtoList(Stream<Book> books) {
+    public List<BookMainInfoDTO> bookStreamToBookDtoList(Stream<Book> books) {
         return books.map(book -> {
                     BookMainInfoDTO bookResponse = modelMapper.map(book, BookMainInfoDTO.class);
                     List<String> types = new ArrayList<>();
 
                     if (!book.getPaperBooks().isEmpty()) {
-                        types.add("Paper book");
+                        for (PaperBook pb : book.getPaperBooks()) {
+                            if (!pb.getIsHidden()) {
+                                types.add("Paper book");
+                                break;
+                            }
+                        }
                     }
-                    if (book.getEbook() != null) {
+                    if (book.getEbook() != null && !book.getEbook().getIsHidden()) {
                         types.add("Ebook");
                     }
-                    if (book.getAudioBook() != null) {
-                        types.add("Audio book");
+                    if (book.getAudioBook() != null && !book.getAudioBook().getIsHidden()) {
+                        types.add("Audiobook");
                     }
 
                     String coverImageUrl = null;
@@ -133,7 +176,7 @@ public class BookService {
                         coverImageUrl = book.getPaperBooks().get(0).getCoverImageUrl();
                     } else if (types.contains("Ebook")) {
                         coverImageUrl = book.getEbook().getCoverImageUrl();
-                    } else if (types.contains("Audio book")) {
+                    } else if (types.contains("Audiobook")) {
                         coverImageUrl = book.getAudioBook().getCoverImageUrl();
                     }
                     bookResponse.setCoverImageUrl(coverImageUrl);
@@ -143,4 +186,96 @@ public class BookService {
                 .toList();
     }
 
+    public byte[] getBookCoverImage(Long id, String bookType, Long paperBookId) {
+        Book book = bookRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Book", "id", id));
+        String imagePath = null;
+        switch (bookType) {
+            case "Paper book" -> {
+                PaperBook paperBook;
+                if (paperBookId != null) {
+                    paperBook = paperBookRepository.findById(paperBookId)
+                            .orElseThrow(() -> new ResourceNotFoundException("Paper book", "id", id));
+                    if (paperBook.getBook() != book) {
+                        throw new ApiRequestException("Invalid paper book id", HttpStatus.BAD_REQUEST);
+                    }
+                } else {
+                    paperBook = book.getPaperBooks().get(0);
+                    if (paperBook == null) {
+                        throw new ApiRequestException("Book with id " + id + " does not have paper version", HttpStatus.BAD_REQUEST);
+                    }
+                }
+                imagePath = paperBook.getCoverImageUrl();
+            }
+            case "Ebook" -> {
+                EBook ebook = book.getEbook();
+                if (ebook == null) {
+                    throw new ApiRequestException("Given book doesn't have an ebook", HttpStatus.BAD_REQUEST);
+                }
+                imagePath = ebook.getCoverImageUrl();
+            }
+            case "Audiobook" -> {
+                AudioBook audiobook = book.getAudioBook();
+                if (audiobook == null) {
+                    throw new ApiRequestException("Given book doesn't have an audiobook", HttpStatus.BAD_REQUEST);
+                }
+                imagePath = audiobook.getCoverImageUrl();
+            }
+        }
+        try {
+            Path path = Paths.get(imagePath);
+            return Files.readAllBytes(path);
+        } catch (IOException e) {
+            throw new ApiRequestException("Error getting image: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    public List<BookMainInfoDTO> getPopularBooks() {
+        List<Book> allBooks = bookRepository.findByPaperBooks_IsHiddenFalseOrAudioBook_IsHiddenFalseOrEbook_IsHiddenFalse();
+        LocalDate oneWeekAgo = LocalDate.now().minusDays(7);
+
+        Map<Book, Double> popularityMap = allBooks.stream()
+                .collect(Collectors.toMap(
+                        book -> book,
+                        book -> {
+                            int numberOfOrders = orderRepository.findByOrderItems_BookAndOrderDateAfter(book, oneWeekAgo).size();
+                            int numberOfWishlists = wishlistRepository.findByBook(book).size();
+
+                            double orderWeight = 0.7;
+                            double wishlistWeight = 0.3;
+
+                            return (orderWeight * numberOfOrders) + (wishlistWeight * numberOfWishlists);
+                        }
+                ));
+
+        List<Book> popularBooks = popularityMap.entrySet().stream()
+                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                .limit(16) // Adjust the limit based on your requirement
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toList());
+
+        List<BookMainInfoDTO> result = bookStreamToBookDtoList(popularBooks.stream());
+
+        return result;
+    }
+
+    public List<BookMainInfoDTO> getRecommendedBooks() {
+        recommendationService.updateRecommendations();
+        try {
+            User user = userService.getCurrentUser();
+            List<BookMainInfoDTO> recommendedBooks = bookStreamToBookDtoList(user.getRecommendedBooks().stream());
+            if (recommendedBooks.size() == 0) {
+                return getPopularBooks();
+            }
+            return recommendedBooks;
+        } catch (Exception e) {
+        }
+        return getPopularBooks();
+    }
+
+    public List<CategoryDTO> getAllCategories() {
+        List<Category> categories = categoryRepository.findAll();
+        List<CategoryDTO> categoryDTOS = categories.stream().map((element) -> modelMapper.map(element, CategoryDTO.class)).collect(Collectors.toList());
+        return categoryDTOS;
+    }
 }
